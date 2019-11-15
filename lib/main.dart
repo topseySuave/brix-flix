bbfe619719478cb927fed6e588a1bfea4b8ab1e6
topseySuave <gabrielsuave17@gmail.com>
2019-11-15 18:06:49 +0100

    remove the custom build carousel widget from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -117,37 +117,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
-
-//  Widget _buildCarousel(BuildContext context, int carouselIndex) {
-//    return Column(
-//      mainAxisSize: MainAxisSize.min,
-//      children: <Widget>[
-//        Text('Carousel $carouselIndex'),
-//        SizedBox(
-//          // you may want to use an aspect ratio here for tablet support
-//          height: 450.0,
-//          child: PageView.builder(
-//            // store this controller in a State to save the carousel scroll position
-//            controller: PageController(viewportFraction: 0.8),
-//            itemBuilder: (BuildContext context, int itemIndex) {
-//              return _buildCarouselItem(context, carouselIndex, itemIndex);
-//            },
-//          ),
-//        )
-//      ],
-//    );
-//  }
-//
-//  Widget _buildCarouselItem(BuildContext context, int carouselIndex, int itemIndex) {
-//    return Padding(
-//      padding: EdgeInsets.symmetric(horizontal: 4.0),
-//      child: Container(
-//        decoration: BoxDecoration(
-//          color: Colors.black,
-//          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-//        ),
-//      ),
-//    );
-//  }
-
 }

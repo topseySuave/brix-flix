19e99c026fd6eb07c410ef56239f9c64ef129eed
topseySuave <gabrielsuave17@gmail.com>
2020-01-30 21:44:10 +0100

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Brix Flix',
+      debugShowCheckedModeBanner: false,
       theme: new ThemeData(
         primarySwatch: Colors.grey,
         accentColor: Colors.black,
